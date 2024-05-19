fd0543f5e729ae8d1268c3ff3118b1e646288c1d
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-05-19 21:30:35 +0200

    creation transporteurs dans easy admin

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,17 +12,32 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProductController extends AbstractController
 {
     #[Route('/produit/{slug}', name: 'app_product')]
-    public function index($slug, ProductRepository $productRepository): Response
-    {
-        // dd($slug);
-        $product = $productRepository->findOneBySlug($slug);
-        // dd($product);
+    // public function index($slug, ProductRepository $productRepository): Response
+    // {
+    //     // dd($slug);
+    //     $product = $productRepository->findOneBySlug($slug);
+    //     // dd($product);
 
-        if(!$product){
+    //     if(!$product){
+    //         return $this->redirectToRoute('app_home');
+    //     }
+    //     return $this->render('product/index.html.twig', [
+    //         'product' => $product,
+    //     ]);
+    // }
+
+    // nouvelle fonctinnalité AUTOMAPPING
+    // ne pas appeler  use Symfony\Bridge\Doctrine\Attribute\MapEntity; sinon ça plante ...
+    //  
+
+    public function index( #[MapEntity(mapping: ['slug' => 'slug'])] Product $product):Response
+    {
+        if (!$product) {
             return $this->redirectToRoute('app_home');
         }
         return $this->render('product/index.html.twig', [
             'product' => $product,
         ]);
+
     }
 }
