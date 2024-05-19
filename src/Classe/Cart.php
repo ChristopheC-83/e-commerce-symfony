c5b7a0b783643a8ebeed7fc6c47fd3b8e3d1759b
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-05-19 08:22:42 +0200

    gestion qtt des produits dans panier

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -13,37 +13,49 @@ class Cart
 
     public function add($product)
     {
-
-
         // appel session
         $cart = $this->requestStack->getSession()->get('cart');
-
         // qtt + 1 
-        if(!empty($cart[$product->getId()])) {
+        if (isset($cart[$product->getId()])) {
             $cart[$product->getId()]['qtt']++;
             $this->requestStack->getSession()->set('cart', $cart);
-        }else{
+        } else {
             $cart[$product->getId()] = [
                 'object' => $product,
                 'qtt' => 1
             ];
             $this->requestStack->getSession()->set('cart', $cart);
-        
-        
         }
 
         // Créer session cart
         $this->requestStack->getSession()->set('cart', $cart);
-
         // dd($this->requestStack->getSession()->get('cart'));
-
     }
 
-    public function  getCart(){ 
+    public function decrease($id)
+    {
+        $cart = $this->requestStack->getSession()->get('cart');
+
+        if ($cart[$id]['qtt'] > 1) {
+            $cart[$id]['qtt']--;
+        } else {
+            unset($cart[$id]);
+        }
+        
+        $this->requestStack->getSession()->set('cart', $cart);
+    }
+
+    public function getCart()
+    {
         return $this->requestStack->getSession()->get('cart');
     }
 
-    public function deleteCart(){
+    public function deleteCart()
+    {
+        return $this->requestStack->getSession()->remove('cart');
+    }
+    public function remove()
+    {
         return $this->requestStack->getSession()->remove('cart');
     }
 
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Classe\Cart;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -21,8 +22,9 @@ class CartController extends AbstractController
     public function delete(Cart $cart): Response
     {
         $cart->deleteCart();
-        $this -> addFlash(
-            'success', "Panier vidé."
+        $this->addFlash(
+            'success',
+            "Panier vidé."
         );
         // dd($cart->getCart());
 
@@ -30,22 +32,46 @@ class CartController extends AbstractController
     }
 
     #[Route('/cart/add/{id}', name: 'app_cart_add')]
-    public function add($id, Cart $cart, ProductRepository $productRepository): Response
+    public function add($id, Cart $cart, ProductRepository $productRepository, Request $request): Response
     {
         // dd($id);
         // panier en DB, pas pertinent
         // on va mettre dans une session
+
+        // dd($request->headers->get('referer'));
+
         $product = $productRepository->findOneById($id);
         $cart->add($product);
 
-        $this -> addFlash(
-            'success', "Produit ajouté au panier."
+        $this->addFlash(
+            'success',
+            "Produit ajouté au panier."
         );
+        return $this->redirect($request->headers->get('referer'));
+    }
 
-        return $this->redirectToRoute('app_product', [
-            'slug' => $product->getSlug(),
-        ]);
+    #[Route('/cart/decrease/{id}', name: 'app_cart_decrease')]
+    public function decrease($id, Cart $cart): Response
+    {
+        $cart->decrease($id);
+
+        $this->addFlash(
+            'warning',
+            "Produit supprimé du panier."
+        );
+        return $this->redirectToRoute('app_cart');
+    }
 
 
+    #[Route('/cart/remove', name: 'app_cart_remove')]
+    public function remove(Cart $cart): Response
+    {
+        $cart->remove();
+
+        $this->addFlash(
+            'secondary',
+            "Le panier est bien vidé !"
+        );
+        return $this->redirectToRoute('app_home');
     }
 }
